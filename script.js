c3bcb4c15a6381d33c2d958f0d10dbabbc61df34
erick <remot>
2023-05-09 12:18:18 -0300

    ej operaciones basicas 1 avanzado

---
+++ script.js
@@ -0,0 +1,19 @@
+const pContador = document.querySelector("#contador");
+let contador = 0;
+
+function aumentarContador(){
+    contador = contador + 1;
+    pContador.innerHTML = contador;
+}
+
+
+function reiniciarContador(){
+    contador = 0;
+    pContador.innerHTML = contador;
+}
+
+
+function restarClickContador(){
+    contador = contador - 1;
+    pContador.innerHTML = contador;
+}
